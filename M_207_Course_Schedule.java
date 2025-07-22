b3c60ce14720ec3adfec058a09adc78a8f07275c
udaloi <pavel.udaloi@innowise-group.com>
2025-07-22 23:28:37 +0300

    https://leetcode.com/problems/course-schedule/ ВАРИАНТ 2

--- M_207_Course_Schedule.java
+++ M_207_Course_Schedule.java
@@ -1,47 +1,117 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.Stack;
 
 public class M_207_Course_Schedule {
 
     public static void main(String[] args) {
         int[][] ints = {{1, 0}};
-        System.out.println(new M_207_Course_Schedule().canFinish(2, ints));
+        System.out.println(new M_207_Course_Schedule().canFinish(2, ints)); // true
 
         int[][] prerequisites2 = {{1, 0}, {0, 1}};
         System.out.println(new M_207_Course_Schedule().canFinish(2, prerequisites2)); // false
+
+        int[][] prerequisites3 = {{1, 4}, {2, 4}, {3, 1}, {3, 2}};
+        System.out.println(new M_207_Course_Schedule().canFinish(5, prerequisites3)); // true
+
+        int[][] prerequisites4 = {{1, 0}, {2, 0}, {0, 1}};
+        System.out.println(new M_207_Course_Schedule().canFinish(3, prerequisites4)); // false
     }
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] cources = new int[numCourses];
+        if (numCourses == 1 || prerequisites.length == 0) {
+            return true;
+        }
+
+        Map<Integer, Node> mapPrerequisites = new HashMap<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            mapPrerequisites.put(i, new Node());
+        }
+
         for (int[] prerequisite : prerequisites) {
-            cources[prerequisite[0]]++;
+            mapPrerequisites.get(prerequisite[1]).routes.add(prerequisite[0]);
         }
 
-        List<Integer> spazy = new LinkedList<>();
-        for (int i = 0; i < cources.length; i++) {
-            if (cources[i] == 0) {
-                spazy.add(i);
+        for (int i = 0; i < numCourses; i++) {
+            Node currentNode = mapPrerequisites.get(i);
+
+            if (Objects.equals(currentNode.status, "watched")) {
+                continue;
             }
-        }
 
-        int finishedCourses = 0;
+            Stack<Node> stack = new Stack<>();
+            stack.push(currentNode);
 
-        while (!spazy.isEmpty()) {
-            Integer i = spazy.removeFirst();
-            finishedCourses++;
-            for (int[] prerequisite : prerequisites) {
-                if (prerequisite[1] == i) {
-                    cources[prerequisite[0]]--;
-                    if (cources[prerequisite[0]] == 0) {
-                        spazy.add(prerequisite[0]);
+            while (!stack.isEmpty()) {
+                Node node = stack.peek();
+
+                if (Objects.equals(node.status, "watched")) {
+                    stack.pop();
+                    continue;
+                }
+
+                if (Objects.equals(node.status, "watching")) {
+                    node.status = "watched";
+                    stack.pop();
+                    continue;
+                }
+
+                node.status = "watching";
+                for (Integer next : node.routes) {
+                    Node nextNode = mapPrerequisites.get(next);
+
+                    if (Objects.equals(nextNode.status, "watching")) {
+                        return false;
+                    }
+
+                    if (!Objects.equals(nextNode.status, "watched")) {
+                        stack.push(nextNode);
                     }
                 }
             }
         }
-
-        return finishedCourses == numCourses;
+        return true;
     }
+
+    public static class Node {
+        public String status = "unwatched";
+        public List<Integer> routes = new ArrayList<>();
+    }
+
+//    public boolean canFinish(int numCourses, int[][] prerequisites) {
+//        int[] cources = new int[numCourses];
+//        for (int[] prerequisite : prerequisites) {
+//            cources[prerequisite[0]]++;
+//        }
+//
+//        List<Integer> spazy = new LinkedList<>();
+//        for (int i = 0; i < cources.length; i++) {
+//            if (cources[i] == 0) {
+//                spazy.add(i);
+//            }
+//        }
+//
+//        int finishedCourses = 0;
+//
+//        while (!spazy.isEmpty()) {
+//            Integer i = spazy.removeFirst();
+//            finishedCourses++;
+//            for (int[] prerequisite : prerequisites) {
+//                if (prerequisite[1] == i) {
+//                    cources[prerequisite[0]]--;
+//                    if (cources[prerequisite[0]] == 0) {
+//                        spazy.add(prerequisite[0]);
+//                    }
+//                }
+//            }
+//        }
+//
+//        return finishedCourses == numCourses;
+//    }
 }
