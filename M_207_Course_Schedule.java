4b42f507961505983cefd21e62d8524b8d3ce833
udaloi <pavel.udaloi@innowise-group.com>
2025-07-27 16:44:56 +0300

    try 3

--- M_207_Course_Schedule.java
+++ M_207_Course_Schedule.java
@@ -1,11 +1,7 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
-import java.util.Stack;
 
 public class M_207_Course_Schedule {
 
@@ -23,68 +19,86 @@ public class M_207_Course_Schedule {
         System.out.println(new M_207_Course_Schedule().canFinish(3, prerequisites4)); // false
     }
 
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (numCourses == 1 || prerequisites.length == 0) {
-            return true;
-        }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {  // todo 3 variant
+        return true;
+    }
 
-        Map<Integer, Node> mapPrerequisites = new HashMap<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            mapPrerequisites.put(i, new Node());
-        }
-
-        for (int[] prerequisite : prerequisites) {
-            mapPrerequisites.get(prerequisite[1]).routes.add(prerequisite[0]);
-        }
-
-        for (int i = 0; i < numCourses; i++) {
-            Node currentNode = mapPrerequisites.get(i);
-
-            if (Objects.equals(currentNode.status, "watched")) {
-                continue;
-            }
-
-            Stack<Node> stack = new Stack<>();
-            stack.push(currentNode);
-
-            while (!stack.isEmpty()) {
-                Node node = stack.peek();
-
-                if (Objects.equals(node.status, "watched")) {
-                    stack.pop();
-                    continue;
-                }
-
-                if (Objects.equals(node.status, "watching")) {
-                    node.status = "watched";
-                    stack.pop();
-                    continue;
-                }
-
-                node.status = "watching";
-                for (Integer next : node.routes) {
-                    Node nextNode = mapPrerequisites.get(next);
-
-                    if (Objects.equals(nextNode.status, "watching")) {
-                        return false;
-                    }
-
-                    if (!Objects.equals(nextNode.status, "watched")) {
-                        stack.push(nextNode);
-                    }
-                }
-            }
-        }
+    public boolean rec() {
         return true;
     }
 
     public static class Node {
         public String status = "unwatched";
-        public List<Integer> routes = new ArrayList<>();
+        public Set<Node> neighbors = new HashSet<>();
+
+        public Node(String status, Set<Node> neighbors) {
+            this.status = status;
+            this.neighbors = neighbors;
+        }
     }
 
-//    public boolean canFinish(int numCourses, int[][] prerequisites) {
+//    public boolean canFinish(int numCourses, int[][] prerequisites) {  todo 1 variant
+//        if (numCourses == 1 || prerequisites.length == 0) {
+//            return true;
+//        }
+//
+//        Map<Integer, Node> mapPrerequisites = new HashMap<>();
+//
+//        for (int i = 0; i < numCourses; i++) {
+//            mapPrerequisites.put(i, new Node());
+//        }
+//
+//        for (int[] prerequisite : prerequisites) {
+//            mapPrerequisites.get(prerequisite[1]).routes.add(prerequisite[0]);
+//        }
+//
+//        for (int i = 0; i < numCourses; i++) {
+//            Node currentNode = mapPrerequisites.get(i);
+//
+//            if (Objects.equals(currentNode.status, "watched")) {
+//                continue;
+//            }
+//
+//            Stack<Node> stack = new Stack<>();
+//            stack.push(currentNode);
+//
+//            while (!stack.isEmpty()) {
+//                Node node = stack.peek();
+//
+//                if (Objects.equals(node.status, "watched")) {
+//                    stack.pop();
+//                    continue;
+//                }
+//
+//                if (Objects.equals(node.status, "watching")) {
+//                    node.status = "watched";
+//                    stack.pop();
+//                    continue;
+//                }
+//
+//                node.status = "watching";
+//                for (Integer next : node.routes) {
+//                    Node nextNode = mapPrerequisites.get(next);
+//
+//                    if (Objects.equals(nextNode.status, "watching")) {
+//                        return false;
+//                    }
+//
+//                    if (!Objects.equals(nextNode.status, "watched")) {
+//                        stack.push(nextNode);
+//                    }
+//                }
+//            }
+//        }
+//        return true;
+//    }
+//
+//    public static class Node {
+//        public String status = "unwatched";
+//        public List<Integer> routes = new ArrayList<>();
+//    }
+
+//    public boolean canFinish(int numCourses, int[][] prerequisites) {         todo 2 variant
 //        int[] cources = new int[numCourses];
 //        for (int[] prerequisite : prerequisites) {
 //            cources[prerequisite[0]]++;
